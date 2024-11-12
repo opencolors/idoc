dc70e1f332607931a40ec7c8b186ccdc9e668517
xsh613 <xsh613@foxmail.com>
2024-11-12 11:11:56 +0800

    feat: add es

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 import shared from './shared.ts'
 import { en } from './en'
 import { zh } from './zh'
+import { es } from './es'
 
 export default defineConfig({
   ...shared,
@@ -14,6 +15,10 @@ export default defineConfig({
     zh: {
       label: '简体中文',
       ...zh,
+    },
+    es: {
+      label: 'Español',
+      ...es,
     },
   },
 })
